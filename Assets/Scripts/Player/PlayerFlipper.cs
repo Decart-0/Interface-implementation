cdca5d7a948ca12471e0cc36568ae1800a38512d
Decart-0 <165915042+Decart-0@users.noreply.github.com>
2025-03-14 01:16:44 +0300

    Update PlayerFlipper.cs
    удалил плавный поворот

--- Assets/Scripts/Player/PlayerFlipper.cs
+++ Assets/Scripts/Player/PlayerFlipper.cs
@@ -5,12 +5,9 @@ public class PlayerFlipper : MonoBehaviour
     private const float AngleRight = 0f;
     private const float AngleLeft = 180f;
 
-    [SerializeField] private float _rotationSpeed;
-
     public void Flip(float direction)
     {
         float targetAngle = direction > 0 ? AngleRight : AngleLeft;
-        Quaternion targetRotation = Quaternion.Euler(new Vector2(0, targetAngle));
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed);
+        transform.rotation = Quaternion.Euler(new Vector2(0, targetAngle));
     }
 }
